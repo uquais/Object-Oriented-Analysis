b21faeb89725bf19e9d458855f14eafd9eec736f
Umair Quais <53208396+uquais@users.noreply.github.com>
2020-02-16 01:36:15 +0530

    Final Spec Commit

--- src/FindGuitarTester.java
+++ src/FindGuitarTester.java
@@ -1,10 +1,6 @@
-import enums.eBuilder;
-import enums.eType;
-import enums.eWood;
+import enums.*;
 
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
+import java.util.*;
 
 public class FindGuitarTester {
 
@@ -12,29 +8,28 @@ public class FindGuitarTester {
         //GuitarSpec searchSpec = new GuitarSpec();
 
         Inventory inventory = new Inventory();
-        initializeInventory(inventory);
-        GuitarSpec WhatEricLikes = new GuitarSpec(eBuilder.fender,eType.acoustic,eWood.alder,eWood.alder);
-        List<Guitar> matchingguitar = inventory.Search(WhatEricLikes);
-        if (!matchingguitar.isEmpty()){
+        FindGuitarTester findGuitarTester = new FindGuitarTester();
+
+        GuitarSpec whatEricLikes = new GuitarSpec(eBuilder.fender, eType.acoustic, eWood.alder, eWood.alder);
+        List<Guitar> matchingGuitar = inventory.Search(whatEricLikes);
+        if (!matchingGuitar.isEmpty()) {
             System.out.println("you might likes these guitars");
-            for (Iterator i =matchingguitar.iterator();i.hasNext();){
-                Guitar guitar =(Guitar)i.next();
-                System.out.println("we have a "+guitar.getSpec().getBuilder()+" "+guitar.getModel()+" "+
-                        guitar.getSpec().getBackWood()+" "+guitar.getSpec().getTopWood()+" "+guitar.getPrice()+"-------");
+            for (Iterator i = matchingGuitar.iterator(); i.hasNext(); ) {
+                Guitar guitar = (Guitar) i.next();
+                System.out.println("we have a " + guitar.getSpec().getBuilder() + " " + guitar.getModel() + " " +
+                        guitar.getSpec().getBackWood() + " " + guitar.getSpec().getTopWood() + " " + guitar.getPrice() + "-------");
             }
-            }
-        else
+        } else
             System.out.println("Sorry, we have nothing for you");
-        }
+    }
 
-
-    public static void initializeInventory(Inventory inventory) {
-        GuitarSpec spec1 = new GuitarSpec(eBuilder.fender,eType.acoustic,eWood.alder,eWood.alder);
-        GuitarSpec spec2 = new GuitarSpec(eBuilder.fender,eType.acoustic,eWood.alder,eWood.indian_rosewood);
-        GuitarSpec spec3 = new GuitarSpec(eBuilder.fender,eType.acoustic,eWood.alder,eWood.alder);
-        inventory.addGuitar("1233","2013",2344,spec1);
-        inventory.addGuitar("1233","2013",2344,spec2);
-        inventory.addGuitar("334", "2010", 455770,spec3);
+    public void initializeInventory(Inventory inventory) {
+        GuitarSpec spec1 = new GuitarSpec(eBuilder.fender, eType.acoustic, eWood.alder, eWood.alder);
+        GuitarSpec spec2 = new GuitarSpec(eBuilder.fender, eType.acoustic, eWood.alder, eWood.indian_rosewood);
+        GuitarSpec spec3 = new GuitarSpec(eBuilder.fender, eType.acoustic, eWood.alder, eWood.alder);
+        inventory.addGuitar("1233", "2013", 2344, spec1);
+        inventory.addGuitar("1233", "2013", 2344, spec2);
+        inventory.addGuitar("334", "2010", 455770, spec3);
 
     }
 
